de12b3cd7e3ab22a686af8a2cf4763cf7af91fa6
Mustafiz82 <mustafiz8260@gmail.com0>
2024-10-15 17:21:20 +0600

    fixed ui mistake

--- app/Components/SponosrCategory.jsx
+++ app/Components/SponosrCategory.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const SponosrCategory = ({ categoryName }) => {
   return (
     <div>
-      <h1 className="text-center my-2 text-xl">{categoryName}</h1>
+      <h1 className="text-center my-2 text-md 2xl:text-xl">{categoryName}</h1>
       <div className="flex flex-col md:flex-row justify-between gap-5 md:gap-10">
-        <div className="bg-[#F8F7FA] w-full md:max-w-fit   p-2 px-8">
+        <div className="bg-[#F8F7FA] border w-full md:max-w-fit   p-2 px-8">
           <div className=" flex items-center  h-full  gap-2 ">
             <Image
               src="/images/orbit.png"
@@ -14,10 +14,10 @@ const SponosrCategory = ({ categoryName }) => {
               width={75}
               height={75}
             ></Image>
-            <h1 className="text-xl">Datalink Limited</h1>
+            <h1 className="text-md 2xl:text-xl">Datalink Limited</h1>
           </div>
         </div>
-        <div className="bg-[#F8F7FA]  w-full md:max-w-fit p-2 px-8">
+        <div className="bg-[#F8F7FA] border  w-full md:max-w-fit p-2 px-8">
           <div className=" flex items-center  h-full  gap-2 ">
             <Image
               src="/images/orbit.png"
@@ -25,10 +25,10 @@ const SponosrCategory = ({ categoryName }) => {
               width={75}
               height={75}
             ></Image>
-            <h1 className="text-xl">Datalink Limited</h1>
+            <h1 className="text-md 2xl:text-xl">Datalink Limited</h1>
           </div>
         </div>
-        <div className="bg-[#F8F7FA]  w-full md:max-w-fit p-2 px-8 ">
+        <div className="bg-[#F8F7FA] border  w-full md:max-w-fit p-2 px-8 ">
           <div className=" flex items-center  h-full  gap-2 ">
             <Image
               src="/images/orbit.png"
@@ -36,7 +36,7 @@ const SponosrCategory = ({ categoryName }) => {
               width={75}
               height={75}
             ></Image>
-            <h1 className="text-xl">Datalink Limited</h1>
+            <h1 className="text-md 2xl:text-xl">Datalink Limited</h1>
           </div>
         </div>
       </div>
